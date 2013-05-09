aadcee5c53c3c351ba9941b903b1118d3dd5ec01
Rutger <rutger@rutger.fedora.imac>
2013-05-09 07:19:16 +0200

    restore of latest stable version

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 var config = {
     general: {
-
         homepage_id: 'map',
         data_url: 'http://www.sneaker-portal.nl/philips/getData',
         newdata_url: 'http://www.sneaker-portal.nl/philips/checkNewData',
--- js/store/websql_store.js
+++ js/store/websql_store.js
@@ -21,14 +21,8 @@ var WebSqlStore = function(successCallback, errorCallback) {
 
     this.createCacheTable = function(tx) {
         var sql = "CREATE TABLE IF NOT EXISTS cache ( " +
-<<<<<<< HEAD
             "cachekey VARCHAR(250) PRIMARY KEY, " +
             "value VARCHAR(1000000))";
-=======
-            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "filterkey VARCHAR(250), " +
-            "value VARCHAR(MAX)";
->>>>>>> 8e9c9d2... created base structure for the app. Config file added and also web-sql storage support.
         tx.executeSql(sql, null,
                 function() {
                     console.log('Create table success');
@@ -37,7 +31,6 @@ var WebSqlStore = function(successCallback, errorCallback) {
                     alert('Create table error: ' + error.message);
                 });
     }
-<<<<<<< HEAD
     
     this.createSettingsTable = function(tx) {
         var sql = "CREATE TABLE IF NOT EXISTS settings ( " +
@@ -80,44 +73,6 @@ var WebSqlStore = function(successCallback, errorCallback) {
         );
     }
     
-=======
-
-    this.addSampleData = function(tx, employees) {
-        var employees = [
-                {"id": 1, "firstName": "Ryan", "lastName": "Howard", "title":"Vice President, North East", "managerId": 0, "city":"New York, NY", "cellPhone":"212-999-8888", "officePhone":"212-999-8887", "email":"ryan@dundermifflin.com"},
-                {"id": 2, "firstName": "Michael", "lastName": "Scott", "title":"Regional Manager", "managerId": 1, "city":"Scranton, PA", "cellPhone":"570-865-2536", "officePhone":"570-123-4567", "email":"michael@dundermifflin.com"},
-                {"id": 3, "firstName": "Dwight", "lastName": "Schrute", "title":"Assistant Regional Manager", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-865-1158", "officePhone":"570-843-8963", "email":"dwight@dundermifflin.com"},
-                {"id": 4, "firstName": "Jim", "lastName": "Halpert", "title":"Assistant Regional Manager", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-865-8989", "officePhone":"570-968-5741", "email":"dwight@dundermifflin.com"},
-                {"id": 5, "firstName": "Pamela", "lastName": "Beesly", "title":"Receptionist", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-999-5555", "officePhone":"570-999-7474", "email":"pam@dundermifflin.com"},
-                {"id": 6, "firstName": "Angela", "lastName": "Martin", "title":"Senior Accountant", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-555-9696", "officePhone":"570-999-3232", "email":"angela@dundermifflin.com"},
-                {"id": 7, "firstName": "Kevin", "lastName": "Malone", "title":"Accountant", "managerId": 6, "city":"Scranton, PA", "cellPhone":"570-777-9696", "officePhone":"570-111-2525", "email":"kmalone@dundermifflin.com"},
-                {"id": 8, "firstName": "Oscar", "lastName": "Martinez", "title":"Accountant", "managerId": 6, "city":"Scranton, PA", "cellPhone":"570-321-9999", "officePhone":"570-585-3333", "email":"oscar@dundermifflin.com"},
-                {"id": 9, "firstName": "Creed", "lastName": "Bratton", "title":"Quality Assurance", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-222-6666", "officePhone":"570-333-8585", "email":"creed@dundermifflin.com"},
-                {"id": 10, "firstName": "Andy", "lastName": "Bernard", "title":"Sales Director", "managerId": 4, "city":"Scranton, PA", "cellPhone":"570-555-0000", "officePhone":"570-646-9999", "email":"andy@dundermifflin.com"},
-                {"id": 11, "firstName": "Phyllis", "lastName": "Lapin", "title":"Sales Representative", "managerId": 10, "city":"Scranton, PA", "cellPhone":"570-241-8585", "officePhone":"570-632-1919", "email":"phyllis@dundermifflin.com"},
-                {"id": 12, "firstName": "Stanley", "lastName": "Hudson", "title":"Sales Representative", "managerId": 10, "city":"Scranton, PA", "cellPhone":"570-700-6464", "officePhone":"570-787-9393", "email":"shudson@dundermifflin.com"},
-                {"id": 13, "firstName": "Meredith", "lastName": "Palmer", "title":"Supplier Relations", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-588-6567", "officePhone":"570-981-6167", "email":"meredith@dundermifflin.com"},
-                {"id": 14, "firstName": "Kelly", "lastName": "Kapoor", "title":"Customer Service Rep.", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-123-9654", "officePhone":"570-125-3666", "email":"kelly@dundermifflin.com"},
-                {"id": 15, "firstName": "Toby", "lastName": "Flenderson", "title":"Human Resources", "managerId": 1, "city":"Scranton, PA", "cellPhone":"570-485-8554", "officePhone":"570-699-5577", "email":"toby@dundermifflin.com"}
-            ];
-        var l = employees.length;
-        var sql = "INSERT OR REPLACE INTO employee " +
-            "(id, firstName, lastName, managerId, title, city, officePhone, cellPhone, email) " +
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        var e;
-        for (var i = 0; i < l; i++) {
-            e = employees[i];
-            tx.executeSql(sql, [e.id, e.firstName, e.lastName, e.managerId, e.title, e.city, e.officePhone, e.cellPhone, e.email],
-                    function() {
-                        console.log('INSERT success');
-                    },
-                    function(tx, error) {
-                        alert('INSERT error: ' + error.message);
-                    });
-        }
-    }
-
->>>>>>> 8e9c9d2... created base structure for the app. Config file added and also web-sql storage support.
     this.findByName = function(searchKey, callback) {
         this.db.transaction(
             function(tx) {
@@ -143,7 +98,6 @@ var WebSqlStore = function(successCallback, errorCallback) {
         );
     }
 
-<<<<<<< HEAD
     this.findCacheKey = function(key, callback) {
         this.db.transaction(
             function(tx) {
@@ -154,20 +108,6 @@ var WebSqlStore = function(successCallback, errorCallback) {
 
                 tx.executeSql(sql, [key], function(tx, results) {
                     callback(results);
-=======
-    this.findById = function(id, callback) {
-        this.db.transaction(
-            function(tx) {
-
-                var sql = "SELECT e.id, e.firstName, e.lastName, e.title, e.city, e.officePhone, e.cellPhone, e.email, e.managerId, m.firstName managerFirstName, m.lastName managerLastName, count(r.id) reportCount " +
-                    "FROM employee e " +
-                    "LEFT JOIN employee r ON r.managerId = e.id " +
-                    "LEFT JOIN employee m ON e.managerId = m.id " +
-                    "WHERE e.id=:id";
-
-                tx.executeSql(sql, [id], function(tx, results) {
-                    callback(results.rows.length === 1 ? results.rows.item(0) : null);
->>>>>>> 8e9c9d2... created base structure for the app. Config file added and also web-sql storage support.
                 });
             },
             function(error) {
@@ -176,7 +116,6 @@ var WebSqlStore = function(successCallback, errorCallback) {
         );
     };
 
-<<<<<<< HEAD
     this.setCacheKey = function(key, value, callback) {
         this.db.transaction(
             function(tx) {
@@ -193,8 +132,6 @@ var WebSqlStore = function(successCallback, errorCallback) {
             }
         );
     };
-=======
->>>>>>> 8e9c9d2... created base structure for the app. Config file added and also web-sql storage support.
     this.initializeDatabase(successCallback, errorCallback);
 
 }
